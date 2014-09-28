9ef66ecf2be3cd9d47a6839486afb40e6b83aac9
witiko <witiko@gmail.com>
2014-09-28 17:41:42 +0200

    shell script update
    The removeDebugger.sh script is now compatible with the 1.3 version

--- shell/removeDebugger.sh
+++ shell/removeDebugger.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 # Removes the debugger object from "$1"
 
-FROM=$( grep -n '^\s*Debugger:\s*$' "$1" | sed 's/:.*//' )
-TO=$( grep -n '^\s*// Additional services\s*$' "$1" | sed 's/:.*//' )
-
-COUNT=1
-IFS=; cat "$1" | while read -r line || [ -n "$line" ]; do
-  if [ $COUNT -lt $FROM ] || [ $COUNT -gt $TO ]; then
-    printf "%s\n" "$line"
-  fi; COUNT=$(( $COUNT + 1 ))
-done | sed 's/return JBus;/return { Scope: JBus.Scope, Node: JBus.Node };/'
+sed 's/return JBus;/return { Scope: JBus.Scope, Node: JBus.Node };/;s/^\(})(\)true\();\)$/\1false\2/' "$1"
